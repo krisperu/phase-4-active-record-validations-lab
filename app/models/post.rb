b1e2c5a479e77e7eeecf0e9102f0e7fa326eacb7
Kristina Peru <krissy.germanov@gmail.com>
2022-04-05 11:49:58 -0600

    commit tree bd25bbd79e5ef01de476cc2fb44410d1551db3ea

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,13 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: ['Fiction', 'Non-Fiction']}
+    validate :must_be_clickbait
+
+    def must_be_clickbait
+        if title.excludes("Won't Believe", "Secret". "Top [number]", "Guess")
+            errors.add(:title, "must be clickbait")
+        end
+    end
 end
